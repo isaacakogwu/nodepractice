e49574fb68eea9539519b688b3d8083e57564348
Isaac Akogwu <isaacakogwu11@gmail.com>
2019-07-23 02:57:53 +0100

    EventEmmitter part

--- app.js
+++ app.js
@@ -1,41 +1,11 @@
-// const path = require('path');
-// const os = require('os');
-// const fs = require('fs');
-
 const EventEmitter = require('events');
 
-const emitter = new EventEmitter();
+const Logger = require('./logger');
+const logger = new Logger();
 
 // Register a Listener
-emitter.on('messageLogged', (arg) => {
+logger.on('messageLogged', (arg) => {
   console.log('Listener called', arg);
 });
 
-// Raise an event
-emitter.emit('messageLogged', {id: 1, url: 'http://...'});
-
-emitter.on('logging', (arg) => {
-  console.log('Logging is also listening', arg);
-});
-
-emitter.emit('logging', {data: 'message'});
-
-// var pathObj = path.parse(__filename);
-// var totalMemory = os.totalmem();
-// var freeMemory = os.freemem();
-
-// var fsFS = fs.readdir('./', function(err, files) {
-//   if (err) console.log('Error', err);
-//   else console.log('Result', files);
-// });
-
-// console.log(pathObj);
-// console.log(totalMemory);
-// console.log(freeMemory);
-
-// console.log(fsFS);
-
-// console.log(`Total Memory: ${totalMemory}`);
-// console.log(`Free Memory: ${freeMemory}`);
-
-
+logger.log('message');
